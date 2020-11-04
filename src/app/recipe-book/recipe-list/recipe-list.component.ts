67c22da8b23f50cba5c8191c7f53eb52183ab39c
Chris <ccalvo@startechup.com>
2020-11-04 12:51:31 +0800

    first commit

---
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe-book.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'Test description', 'https://kbimages1-a.akamaihd.net/d92db093-478c-4ecb-9e6e-f1767eb220b2/1200/1200/False/recipe-book-the-best-food-recipes-that-are-delicious-healthy-great-for-energy-and-easy-to-make.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
