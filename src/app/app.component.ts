9ffd20d84fdc06a3def043aafbe89697f2ba70c1
Chris <ccalvo@startechup.com>
2020-11-05 13:32:42 +0800

    Passing data with event and property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'recipe-shopping-project';
+  loadedFeature = 'recipe-book';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { Recipe } from '../recipe-book.model';
 
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe-book.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'Test description', 'https://kbimages1-a.akamaihd.net/d92db093-478c-4ecb-9e6e-f1767eb220b2/1200/1200/False/recipe-book-the-best-food-recipes-that-are-delicious-healthy-great-for-energy-and-easy-to-make.jpg')
   ];
@@ -15,6 +16,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
